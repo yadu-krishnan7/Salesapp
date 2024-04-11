c0f1caac5f751a7c7d7db4bdec54a7351e6ed608
yadu-krishnan7 <yadhuvasanthigovindan7@gmail.com>
2024-04-11 17:03:56 +0530

    initial commit of sales app

---
+++ salesapp/src/main/java/com/sparksupport/salesapp/SalesappApplication.java
@@ -0,0 +1,13 @@
+package com.sparksupport.salesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesappApplication.class, args);
+	}
+
+}
---
+++ salesapp/src/test/java/com/sparksupport/salesapp/SalesappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparksupport.salesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
